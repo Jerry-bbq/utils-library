df5e360c45ab37a94b433ae77a0af2853d8ec304
lujing <lujing@zhixuezhen.com>
2019-03-26 17:47:53 +0800

    fix: 数组去重

--- dist/iutils.esm.js
+++ dist/iutils.esm.js
@@ -161,11 +161,13 @@ var deepClone = function(obj) {
  * @param {*} arr 
  * @return [arr]
  * 测试数据: [1,2,1,'11','11',true,true,undefined,undefined,null,null,NaN,NaN,{},{},{a:1},{a:1}]
- * 问题: NaN,{},{a:1}并不能实现去重
  */
 var unique = function (arr) {
+    if (!(arr instanceof Array)) {
+        return new Error('参数不合法')
+    }
     var obj = {};
-    return arr.filter(function (item, index, arr) {
+    return arr.filter(function (item) {
         return obj.hasOwnProperty(typeof item + item) ? false : (obj[typeof item + item] = true)
     })
 };
