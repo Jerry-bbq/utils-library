b99a467cc3e2467ec4235a728c00885cb39eda87
lujing <lujing@zhixuezhen.com>
2019-04-01 15:58:29 +0800

    feat: Date对象格式化

--- README.md
+++ README.md
@@ -37,32 +37,36 @@ import { digitUppercase } from 'utils-library'
 
 ### DOM操作相关
 
-* [hasClass](./src/dom/hasClass.js)&emsp;&emsp;是否有class
-* [addClass](./src/dom/addClass.js)&emsp;&emsp;添加class
-* [removeClass](./src/dom/removeClass.js)&emsp;&emsp;删除class
+- [hasClass](./src/dom/hasClass.js)&emsp;&emsp;是否有class
+- [addClass](./src/dom/addClass.js)&emsp;&emsp;添加class
+- [removeClass](./src/dom/removeClass.js)&emsp;&emsp;删除class
 
 ### Cookie操作相关
 
-* [getCookie](./src/cookie/getCookie.js)&emsp;&emsp;获取cookie
-* [setCookie](./src/cookie/setCookie.js)&emsp;&emsp;设置cookie
-* [removeCookie](./src/cookie/removeCookie.js)&emsp;&emsp;删除cookie
+- [getCookie](./src/cookie/getCookie.js)&emsp;&emsp;获取cookie
+- [setCookie](./src/cookie/setCookie.js)&emsp;&emsp;设置cookie
+- [removeCookie](./src/cookie/removeCookie.js)&emsp;&emsp;删除cookie
 
 ### Array
 
-* [deepClone](./src/array/deepClone.js)&emsp;&emsp;数组深拷贝
-* [unique](./src/array/unique.js)&emsp;&emsp;数组去重
+- [deepClone](./src/array/deepClone.js)&emsp;&emsp;数组或对象的深拷贝
+- [unique](./src/array/unique.js)&emsp;&emsp;数组去重
 
 ### Object
 
-* [isEmptyObject](./src/object/isEmptyObject.js)&emsp;&emsp;是否是一个空对象
+- [isEmptyObject](./src/object/isEmptyObject.js)&emsp;&emsp;判断一个对象是否是一个空对象
 
 ### 正则校验
 
-* [checkID](./src/reg/checkID.js)&emsp;&emsp;15位和18位身份证号码验证的校验
-* [checkMobile](./src/reg/mobile.js)&emsp;&emsp;校验严格手机号码
-* [checkCommonMobile](./src/reg/commonMobile.js)&emsp;&emsp;校验常规的手机号码
-* [trim](./src/reg/trim.js)&emsp;&emsp;去字符串的首尾空格
+- [checkID](./src/reg/checkID.js)&emsp;&emsp;15位和18位身份证号码验证的校验
+- [checkMobile](./src/reg/mobile.js)&emsp;&emsp;校验严格手机号码
+- [checkCommonMobile](./src/reg/commonMobile.js)&emsp;&emsp;校验常规的手机号码
+- [trim](./src/reg/trim.js)&emsp;&emsp;去字符串的首尾空格
  
 ### 转换
 
-* [digitUppercase](./src/digitUppercase/digitUppercase.js)&emsp;&emsp;金额阿拉伯数字转换为人民币大写
+- [digitUppercase](./src/digitUppercase/digitUppercase.js)&emsp;&emsp;金额阿拉伯数字转换为人民币大写
+
+### 日期
+
+- [format](./src/date/format.js) &emsp;&emsp;Date对象格式化
--- dist/iutils.esm.js
+++ dist/iutils.esm.js
@@ -167,8 +167,8 @@ var unique = function (arr) {
         return new Error('参数不合法')
     }
     var obj = {};
-    return arr.filter(function (item) {
-        return obj.hasOwnProperty(typeof item + item) ? false : (obj[typeof item + item] = true)
+    return arr.filter(function (v) {
+        return obj.hasOwnProperty(typeof v + v) ? false : (obj[typeof v + v] = true)
     })
 };
 
@@ -181,4 +181,42 @@ function isEmptyObject(obj) {
     return Object.keys(obj).length > 0 ? false : true
 }
 
-export { hasClass, addClass, removeClass, getCookie, setCookie, removeCookie, checkID, checkMobile, checkCommonMobile, deepClone, unique, isEmptyObject };
+/**
+ * 定义一个简单的日期格式化方法
+ * y+: 年份,yy,yyyy,缺点：输入几个y就显示几个y
+ * M+: 月份,M（1-12）,MM（01-12）
+ * d+: 月份中的天数,d(1-31),dd(01-31)
+ * h+: 小时,h(1-24),hh(01-24),缺点：不兼容12小时制
+ * m+: 分钟,h(0-59),m(00-59)
+ * s+: 秒钟,s(0-59),s(00-59)
+ * @param {*} date 
+ * @param {*} format 
+ * @return string
+ */
+var format = function (date, format) {
+    // 定义一个对象，key-年月日时分秒的字符串正则，value-获取到对应的值
+    var o = {
+        "M+": date.getMonth() + 1,
+        "d+": date.getDate(),
+        "h+": date.getHours(),
+        "m+": date.getMinutes(),
+        "s+": date.getSeconds(),
+    };
+    // 正则匹配年份
+    if (/(y+)/.test(format)) { // 如果字符串参数中至少含有一个y
+        // 替换字符串中，正则匹配到的 (y+) 为 获取到的日期对象的年份的子字符串(从4-正则匹配到的字符串的长度开始截取)
+        format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    // 遍历定义的对象
+    for (var k in o) {
+        // 正则匹配定义对象的key值，分别对 年月日时分秒 进行正则匹配
+        if (new RegExp("(" + k + ")").test(format)) {
+            // 如果匹配到的字符串的长度是1，则显示一位，
+            // 否则，用00拼接定义对象的value，获取其子字符串（从其key对应的value的长度为下标开始截取）
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+        }
+    }
+    return format;
+};
+
+export { hasClass, addClass, removeClass, getCookie, setCookie, removeCookie, checkID, checkMobile, checkCommonMobile, deepClone, unique, isEmptyObject, format };
